a81e34788b87fec944dbe51ffa8951c6bf128243
Nishkal <kashyapnishkal@gmail.com>
2019-11-28 16:38:46 +0530

    updated snv files

--- components/src/environments/environment.prod.ts
+++ components/src/environments/environment.prod.ts
@@ -1,22 +1,14 @@
 export const environment = {
   production: true,
   firebase: {
-    // apiKey: 'AIzaSyAesSTpq6RdhSGqqh3BNcJErodzk1ePzps',
-    // authDomain: 'fireship-dev-17429.firebaseapp.com',
-    // databaseURL: 'https://fireship-dev-17429.firebaseio.com',
-    // projectId: 'fireship-dev-17429',
-    // storageBucket: 'fireship-dev-17429.appspot.com',
-    // messagingSenderId: '307044372590',
-    // appId: '1:307044372590:web:1acc71e3f0c7bfbef2967f',
-    // measurementId: 'G-test'
-    apiKey: "AIzaSyBDBbwdU7XHAXwJa91R77jOWNFy6rW4Y_o",
-    authDomain: "diy-mechatronics.firebaseapp.com",
-    databaseURL: "https://diy-mechatronics.firebaseio.com",
-    projectId: "diy-mechatronics",
-    storageBucket: "diy-mechatronics.appspot.com",
-    messagingSenderId: "795809219937",
-    appId: "1:795809219937:web:c6b8b9408f847e18813120",
-    measurementId: "G-EE7GP0QKT2"
+    apiKey: 'AIzaSyBDBbwdU7XHAXwJa91R77jOWNFy6rW4Y_o',
+    authDomain: 'diy-mechatronics.firebaseapp.com',
+    databaseURL: 'https://diy-mechatronics.firebaseio.com',
+    projectId: 'diy-mechatronics',
+    storageBucket: 'diy-mechatronics.appspot.com',
+    messagingSenderId: '795809219937',
+    appId: '1:795809219937:web:c6b8b9408f847e18813120',
+    measurementId: 'G-EE7GP0QKT2'
   },
   // stripe: 'pk_live_qSaGVmF1x4X3vIWZmpbgceNU'
   stripe: 'replace-with-mine'
--- components/src/environments/environment.ts
+++ components/src/environments/environment.ts
@@ -5,14 +5,14 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyBDBbwdU7XHAXwJa91R77jOWNFy6rW4Y_o",
-    authDomain: "diy-mechatronics.firebaseapp.com",
-    databaseURL: "https://diy-mechatronics.firebaseio.com",
-    projectId: "diy-mechatronics",
-    storageBucket: "diy-mechatronics.appspot.com",
-    messagingSenderId: "795809219937",
-    appId: "1:795809219937:web:c6b8b9408f847e18813120",
-    measurementId: "G-EE7GP0QKT2"
+    apiKey: 'AIzaSyBDBbwdU7XHAXwJa91R77jOWNFy6rW4Y_o',
+    authDomain: 'diy-mechatronics.firebaseapp.com',
+    databaseURL: 'https://diy-mechatronics.firebaseio.com',
+    projectId: 'diy-mechatronics',
+    storageBucket: 'diy-mechatronics.appspot.com',
+    messagingSenderId: '795809219937',
+    appId: '1:795809219937:web:c6b8b9408f847e18813120',
+    measurementId: 'G-EE7GP0QKT2'
   },
   // stripe: 'pk_live_qSaGVmF1x4X3vIWZmpbgceNU'
   stripe: 'replace-with-mine'
